d5dc99ea6bb733ba81511b7a1dca3fb29dcfdab2
jhannica2016 <christianaustria167@gmail.com>
2020-03-04 15:21:33 +0800

    delete for size

--- ORJ/fetchAffliate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data='<h4 class="text mt-4" style="color: #763435">Select Affiliation</h4><br>
-         <select class="form-control" id="category" name="category" style="width:250px;">
-            <option selected>Select All</option>
-            <option> Student</option>
-            <option> Faculty</option>
-         </select>';
-	echo json_encode($data);
-?>
--- ORJ/mon/README.md
+++
@@ -1,5 +0,0 @@
-# blob-upload
-This code is all about uploading a file ans store it in the database as a blob file and retrieving the file blob file 
-in appropriate format.
-
-Key Technologies used are PHP,HTML5
