1afa3c5db6f0ec3c3edc59fe9e8bc05bd1fc2c23
louisefaj <louisefaj@gmail.com>
2020-03-12 09:52:50 +0800

    Guest
    login and signup change background

--- register.php
+++ register.php
@@ -11,7 +11,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
         <link rel="stylesheet" href="trytry.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.5.3/dist/sweetalert2.all.min.js"></script>
         <style>
     body{
